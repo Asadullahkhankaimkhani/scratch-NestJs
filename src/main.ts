ced1a2a5ce739b97374effab170630780cffa2a6
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2022-02-28 01:11:36 +0500

    File Conventions

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,6 @@
-import { Controller, Module, Get } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 
-@Controller()
-class AppController {
-  @Get()
-  getRootRoute() {
-    return "Hello World";
-  }
-}
-
-@Module({
-  controllers: [AppController],
-})
-class AppModule {}
+import { AppModule } from "./app.module";
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
